2d9acc3fff0422b3f351ffbf0951d6e07080e63b
intom <iin.arahmah99@gmail.com>
2021-05-23 20:01:12 +0700

    make controller major

---
+++ app/Http/Controllers/MajorsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Major;
+use Illuminate\Http\Request;
+
+class MajorsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $majors = Major::all();
+        return view('majors.index', compact('majors'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Major  $major
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Major $major)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Major  $major
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Major $major)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Major  $major
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Major $major)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Major  $major
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Major $major)
+    {
+        //
+    }
+}
