6a8b9f53b3475fcb16a41239b76555d75af3c471
intom <iin.arahmah99@gmail.com>
2021-05-24 05:42:10 +0700

    modal-form edit course

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -31,9 +31,9 @@
   <i class="bi bi-plus-lg"></i>  Create
 </button>
 
-@if (session('status'))
+@if (session('message'))
     <div class="alert alert-success">
-        {{ session('status') }}
+        {{ session('message') }}
     </div>
 @endif
                                    
@@ -59,7 +59,10 @@
                          <td>{{$row->semester}}</td>
                           
                          <td>
-                             <a href="" class="btn btn-primary"><i class="bi bi-pencil-square"></i> Edit</a>
+                          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editData">
+                            <i class="bi bi-pencil-square"></i> Edit
+                          </button>
+                             
                              <a href="" class="btn btn-danger"><i class="bi bi-trash"></i> Delete</a>
                          </td>
 
@@ -83,12 +86,12 @@
 
 
 
-<!-- Modal -->
+<!-- Modal Create Data-->
 <div class="modal fade" id="createData" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="createDataLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="createDataLabel">Create Course Data</h5>
+        <h5 class="modal-title" id="createDataLabel">Create Data Course</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -98,19 +101,63 @@
           @csrf
           <div class="form-group">
             <label>No. Course</label>
-            <input type="text" name="no_course" class="form-control" placeholder="no course">
+            <input type="text" name="no_course" class="form-control @error('no_course') is-invalid @enderror" value="{{old('no_course')}}" placeholder="input no course">
+            @error('no_course') <div class="invalid-feedback">{{$message}}</div>@enderror
           </div>
           <div class="form-group">
             <label>Course Name</label>
-            <input type="text" name="course_name" class="form-control" placeholder="course name">
+            <input type="text" name="course_name" class="form-control @error('course_name') is-invalid @enderror" value="{{old('course_name')}}" placeholder="input course name">
+            @error('course_name') <div class="invalid-feedback">{{$message}}</div>@enderror
           </div>
           <div class="form-group">
             <label>SKS</label>
-            <input type="text" name="sks" class="form-control" placeholder="sks">
+            <input type="text" name="sks" class="form-control" placeholder="input sks">
           </div>
           <div class="form-group">
             <label>Semester</label>
-            <input type="text" name="semester" class="form-control" placeholder="semester">
+            <input type="text" name="semester" class="form-control" placeholder="input semester">
+          </div>    
+         
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-primary">Save</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal Edit Data-->
+<div class="modal fade" id="editData" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="editDataLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="editDataLabel">Edit Course Data</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="POST" action="{{action('CoursesController@store')}}">
+          @csrf
+          <div class="form-group">
+            <label>No. Course</label>
+            <input type="text" name="no_course" class="form-control @error('no_course') is-invalid @enderror" value="{{old('no_course')}}" placeholder="input no course">
+            @error('no_course') <div class="invalid-feedback">{{$message}}</div>@enderror
+          </div>
+          <div class="form-group">
+            <label>Course Name</label>
+            <input type="text" name="course_name" class="form-control @error('course_name') is-invalid @enderror" value="{{old('course_name')}}" placeholder="input course name">
+            @error('course_name') <div class="invalid-feedback">{{$message}}</div>@enderror
+          </div>
+          <div class="form-group">
+            <label>SKS</label>
+            <input type="text" name="sks" class="form-control" placeholder="input sks">
+          </div>
+          <div class="form-group">
+            <label>Semester</label>
+            <input type="text" name="semester" class="form-control" placeholder="input semester">
           </div>    
          
       </div>
