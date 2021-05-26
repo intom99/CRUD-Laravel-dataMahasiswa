3478239c76e37125a0dc0bb3be86cfbf4a8465fd
intom <iin.arahmah99@gmail.com>
2021-05-26 21:48:25 +0700

    model course: make function student

--- app/Course.php
+++ app/Course.php
@@ -8,4 +8,9 @@ class Course extends Model
 {
 
     protected $fillable = ['no_course', 'course_name', 'sks', 'semester'];
+
+    public function student()
+    {
+        return $this->hasMany('App\Student', 'course_id');
+    }
 }
