e49741e846ed71cbec3d425e66c94acfd3dbeabb
intom <iin.arahmah99@gmail.com>
2021-05-26 21:50:40 +0700

    model major: make function student

--- app/Major.php
+++ app/Major.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Major extends Model
 {
     protected $fillable = ['major_code', 'major_name'];
+
+    public function student()
+    {
+        return $this->hasOne('App\Student', 'major_id');
+    }
 }
