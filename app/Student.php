eeb06caef3d2afdf62c082aeea2637afc944cc98
intom <iin.arahmah99@gmail.com>
2021-05-26 21:51:43 +0700

    model student: make func major and func course

--- app/Student.php
+++ app/Student.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     //
+
+    public function major()
+    {
+        return $this->belongsTo('App\Major', 'major_id');
+    }
+    public function course()
+    {
+        return $this->belongsTo('App\Course', 'course_id');
+    }
 }
