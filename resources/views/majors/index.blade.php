b392e63dbc271e7a55abab1f1057e1005d55f82b
intom <iin.arahmah99@gmail.com>
2021-05-29 00:23:27 +0700

    major: edit data

--- resources/views/majors/index.blade.php
+++ resources/views/majors/index.blade.php
@@ -25,40 +25,39 @@
             <div class="card">
                 <div class="card-body">
                                  
-                                  <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#createData">
-  <i class="bi bi-plus-lg"></i>  Create
-</button>
+         <!-- Button trigger modal -->
+          <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#createData">
+            <i class="bi bi-plus-lg"></i>  Create
+          </button>
+            @if (session('message'))
+                <div class="alert alert-success">
+                    {{ session('message') }}
+                </div>
+            @endif
 
-@if (session('message'))
-    <div class="alert alert-success">
-        {{ session('message') }}
-    </div>
-@endif
-
-@if(!empty($majors))
-                                   
+            @if(!empty($majors))                      
                   <table class="table">
                     <thead class="thead-light">
                       <tr>
                         <th scope="col">#</th>
                         <th scope="col">No Major</th>
                         <th scope="col">Major Name</th>
-                                                <th scope="col">Action</th>
+                        <th scope="col">Action</th>
                       </tr>
                     </thead>
                     <tbody>
                         @foreach($majors as $row)
                         <tr>
-
-                          <th scope="row">{{ ($majors ->currentpage()-1) * $majors ->perpage() + $loop->index + 1 }}</th>
+                         <th scope="row">{{ ($majors ->currentpage()-1) * $majors ->perpage() + $loop->index + 1 }}</th>
                          <td>{{$row->major_code}}</td>
-                         <td>{{$row->major_name}}</td>
-                         
-                          
+                         <td>{{$row->major_name}}</td>                         
                          <td>
-                             <a href="" class="btn btn-primary"><i class="bi bi-pencil-square"></i> Edit</a>
-                             {{-- <a href="" class="btn btn-danger"><i class="bi bi-trash"></i> Delete</a> --}}
+                            
+                             <!-- Button trigger modal -->
+                          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editData">
+                            <i class="bi bi-pencil-square"></i> Edit
+                          </button>
+                                            
                          <form action="{{action('MajorsController@destroy', $row->id)}}" method="post" class="d-inline">
                           @method('delete')
                           @csrf
@@ -113,13 +112,48 @@
         <form method="POST" action="{{action('MajorsController@store')}}">
           @csrf
           <div class="form-group">
-            <label>Major Code</label>
+            <label class="font-weight-bold">Major Code</label>
             <input type="text" name="major_code" class="form-control @error('major_code') is-invalid @enderror" value="{{old('major_code')}}" placeholder="input code">
             @error('major_code') <div class="invalid-feedback">{{$message}}</div>@enderror
           </div>
           <div class="form-group">
-            <label>Major Name</label>
+            <label class="font-weight-bold">Major Name</label>
             <input type="text" name="major_name" class="form-control @error('major_name') is-invalid @enderror" value="{{old('major_name')}}" placeholder="input name">
+            @error('major_name') <div class="invalid-feedback">{{$message}}</div>@enderror
+          </div>     
+         
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-primary">Save</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal Edit Data Major-->
+<div class="modal fade" id="editData" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="editDataLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="editDataLabel">Edit Data Major</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="POST" action="{{action('MajorsController@update', $row->id)}}">
+          @method('PATCH')
+          @csrf
+          <div class="form-group">
+            <label class="font-weight-bold">Major Code</label>
+            <input type="text" name="major_code" class="form-control @error('major_code') is-invalid @enderror" value="{{$row->major_code}}" placeholder="input code">
+            @error('major_code') <div class="invalid-feedback">{{$message}}</div>@enderror
+          </div>
+          <div class="form-group">
+            <label class="font-weight-bold">Major Name</label>
+            <input type="text" name="major_name" class="form-control @error('major_name') is-invalid @enderror" value="{{$row->major_name}}" placeholder="input name">
             @error('major_name') <div class="invalid-feedback">{{$message}}</div>@enderror
           </div>     
          
